aacc2906c2b5d85f62e205e302d556f8f3d6cf51
lenz <lenz1986@gmail.com>
2020-12-10 18:49:24 +0000

    small fix

--- domotics.sh
+++ domotics.sh
@@ -20,8 +20,31 @@ usermod -a -G dialout domoticz
 
 echo "Add plugin"
 cd /etc/domoticz/plugins/
+DIR="Domoticz-Zigate"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "previous installation find remove it"
+  rm -r /etc/domoticz/plugins/Domoticz-Zigate
+fi
 git clone https://github.com/pipiche38/Domoticz-Zigate.git
 chmod +x Domoticz-Zigate/plugin.py
 
+echo "Moving files and download domoticz config"
+
+cd /tmp/
+mv /var/lib/domoticz/domoticz.db /etc/domoticz/domoticz.db
+mv /var/lib/domoticz/domoticz.db-shm /etc/domoticz/domoticz.db-shm
+mv /var/lib/domoticz/domoticz.db-wal /etc/domoticz/domoticz.db-wal
+
+wget https://github.com/lmahmutov/lumi_packages/raw/main/domoticz_etc
+wget https://github.com/lmahmutov/lumi_packages/raw/main/domoticz_init
+
+mv /tmp/domoticz_init /etc/init.d/domoticz
+chmod 755 /etc/init.d/domoticz
+mv /tmp/domoticz_etc /etc/config/domoticz
+chmod 600 /etc/config/domoticz
+
+
 chown -R domoticz:domoticz /etc/domoticz
-echo "Installation complete"
+echo "Installation complete, reboot"
+reboot
